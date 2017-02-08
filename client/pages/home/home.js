a76997ba4afbf2a0b181c4320c0080b5d6425d95
Kian.peymani@gmail.com <Kian.peymani@gmail.com>
2017-02-09 02:07:39 +0330

    noty added + fix header

--- client/pages/home/home.js
+++ client/pages/home/home.js
@@ -1,15 +1,15 @@
 Template.home.onRendered(function () {
   $(window).scroll(function () {
-    if ($(window).scrollTop() == 0) {
-      $('#mainNav').addClass('transp')
-    } else {
-      $('#mainNav').removeClass('transp')
+    if (window.location.pathname === '/') {
+      if ($(window).scrollTop() == 0) {
+        $('#mainNav').addClass('transp')
+      } else {
+        $('#mainNav').removeClass('transp')
+      }
     }
   })
 
   setTimeout(function () {
-    console.log(($('#carousel').height() + 10) + 'px')
-
     $('#content-start').css('margin-top', ($('#carousel').height() + 20) + 'px')
   }, 200)
 })
@@ -31,4 +31,3 @@ Template.home.events({
 
   }
 })
-console.log(Template)
