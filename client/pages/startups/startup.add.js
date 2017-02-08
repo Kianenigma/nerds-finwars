72e558f1c89a1dfb27b884d25c466b647c319d0a
M.n.M <MnM@MnMs-MacBook-Pro.local>
2017-02-09 01:44:22 +0330

    add startup first step data handling

--- client/pages/startups/startup.add.js
+++ client/pages/startups/startup.add.js
@@ -1,9 +1,35 @@
+  let startupData = {};
 Template['startup.add'].onRendered(function () {
   $('#example-vertical').steps({
     headerTag: 'h3',
     bodyTag: 'section',
     transitionEffect: 'slideLeft',
-    stepsOrientation: 'vertical'
+    stepsOrientation: 'vertical',
+    onStepChanging: function (event, currentIndex, newIndex)
+    {
+      if (currentIndex === 0) {
+        startupData = Object.assign(startupData, {name: $('#name').val()});
+        startupData = Object.assign(startupData, {city: $('#city').val()});
+        startupData = Object.assign(startupData, {province: $('#province').val()});
+        startupData = Object.assign(startupData, {detail: $('#detail').val()});
+        startupData = Object.assign(startupData, {category: $('#category').val()});
+      } else if (currentIndex === 1) {
+        
+      } else if (currentIndex === 2) {
+        
+      } else if (currentIndex === 3) {
+        
+      }
+      console.log(startupData);
+      return true
+    },
+    onFinishing: function() {
+      console.log('finishing');
+      return true
+    },
+    onFinished: function() {
+     console.log('calling method')
+    }
   })
 
   $('#projectDatePicker').pDatepicker()
