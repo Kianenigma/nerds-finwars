266f1a2caf424a1850f14e90987cb240a3404e70
AJAND <aj.mardalizad@gmail.com>
2017-02-09 18:47:54 +0330

    search list startup

--- client/pages/startups/startup.list.js
+++ client/pages/startups/startup.list.js
@@ -3,7 +3,10 @@ Template['startup.list'].onCreated(function() {
 })
 Template['startup.list'].helpers({
     'startups': function() {
-        return Startups.find({},{limit: Session.get('paginateListStartup')});
+      var search = {};
+      search.name = {$regex: new RegExp(Session.get('searchListSession')),
+      $options: "i"};
+      return Startups.find(search,{limit: Session.get('paginateListStartup')});
     }
 });
 
@@ -12,5 +15,10 @@ Template['startup.list'].events({
         event.preventDefault();
         Session.set('paginateListStartup', Session.get('paginateListStartup') + 8);
 
-    }
+    },
+
+    "keyup input": _.debounce(function(e) {
+      var source = $('#searchList').val().trim();
+      Session.set('searchListSession', source);
+    }, 200)
 });
