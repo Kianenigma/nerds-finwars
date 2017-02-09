c7e651b2ab175456750f894757b6d1e970618847
Kian.peymani@gmail.com <Kian.peymani@gmail.com>
2017-02-09 17:03:08 +0330

    single page done

--- client/pages/startups/startup.single.js
+++ client/pages/startups/startup.single.js
@@ -1,6 +1,16 @@
 Template['startup.single'].onRendered(function () {
   // $('#overflowBack').css('width', $('#overflowBack').parent().width())
   //   .css('height', $('#overflowBack').parent().height())
+  //
+
+  $('.tab-item').click(function () {
+    $('.tab-item').removeClass('active')
+    $(this).addClass('active')
+  })
+
+  $('.chart').easyPieChart({
+    barColor: '#8E44AD',
+  })
 })
 Template['startup.single'].helpers({
   create: function () {
