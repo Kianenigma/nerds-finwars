e90565e5c885257ade81253ea2a321390cb80704
M.n.M <MnM@MnMs-MacBook-Pro.local>
2017-02-10 10:22:20 +0330

    parseInt invest value

--- client/pages/investest/invest.js
+++ client/pages/investest/invest.js
@@ -11,7 +11,7 @@ Template.invest.helpers({
 Template.invest.events({
     'submit form': function(event) {
         event.preventDefault();
-        var investValue = event.target.investValue.value;
+        var investValue = parseInt(event.target.investValue.value);
         var startupId = this._id;
         Meteor.call('invest.insert', investValue, startupId);
         event.target.investValue.value = "";
