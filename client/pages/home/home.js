e4fe773b1913b58f604b551eae860ce267301229
AJAND <aj.mardalizad@gmail.com>
2017-02-10 10:43:00 +0330

    home startupPic

--- client/pages/home/home.js
+++ client/pages/home/home.js
@@ -11,7 +11,7 @@ Template.home.onRendered(function () {
 
   setTimeout(function () {
     $('#content-start').css('margin-top', ($('#carousel').height() + 20) + 'px')
-  }, 250)
+  }, 25)
 })
 
 Template.home.helpers({
@@ -29,6 +29,10 @@ Template.home.helpers({
 Template.home.events({
   'click #foo': function (event, template) {
 
+  },
+  'click .startupPicBox': function() {
+    console.log('fuck');
+    Router.go('startupSingle', {id: this._id});
   }
 })
 
