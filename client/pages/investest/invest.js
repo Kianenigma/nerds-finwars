04d6ed7e93613a9789dc694c26310fb07d936427
Kian.peymani@gmail.com <Kian.peymani@gmail.com>
2017-02-10 17:53:19 +0330

    invest alert added

--- client/pages/investest/invest.js
+++ client/pages/investest/invest.js
@@ -9,6 +9,7 @@ Template.invest.events({
         var investValue = parseInt(event.target.investValue.value);
         var startupId = this._id;
         Meteor.call('invest.insert', investValue, startupId);
+        sAlert.success('سرمایه شما واریز شد')
         event.target.investValue.value = "";
     }
 });
